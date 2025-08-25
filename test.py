11c1ecab98ca199a31a7d4bd0cef1c1be0f97eb8
humam <humamo2003@gmail.com>
2025-08-25 10:00:35 +0300

    returned the logo to the main stramlit page

--- test.py
+++ test.py
@@ -2,10 +2,13 @@ import pandas as pd
 
 from plotting import create_lineplot
 
-
-
-df = pd.read_csv('processed_microplastics.csv')
+df = pd.read_csv("processed_microplastics.csv")
 
 fruits_trend = df.groupby("year")["fruits"].median().reset_index()
 
-create_lineplot(fruits_trend, 'year', 'fruits', "Global Average Microplastic Content in Fruits (1990–2018)")
+create_lineplot(
+    fruits_trend,
+    "year",
+    "fruits",
+    "Global Average Microplastic Content in Fruits (1990–2018)",
+)
